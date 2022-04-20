c398f55298d80d6a088c26da25ae79a6866c612d
kekoakailiawa <kkailiaw@ucsc.edu>
2022-04-20 13:10:14 -0700

    lab 5 update

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/**
+ * Author:    Kekoa Kailiawa <kkailiaw@ucsc.edu>
+ * Created:   04.19.2022
+ * License:   Public Domain
+ **/
+
+ // Define Variables
+make = "Subaru";
+model = "Impreza";
+color = "grey";
+year = 2020;
+
+ // Calculate
+age = 2022 - year;
+
+ // output
+ document.writeln("make: " + make + "<br>");
+ document.writeln("model: " + model + "<br>");
+ document.writeln("color: " + color + "<br>");
+ document.writeln("year: " + year + "<br>");
+ document.writeln("age: " + age + "years<br>");
