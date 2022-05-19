27759d05de71136729e70a80f4e4d7115e4a3b08
kekoakailiawa <kkailiaw@ucsc.edu>
2022-05-19 08:35:51 -0700

    Update lab.js

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -9,31 +9,24 @@ function fizzBuzzBoom () {
       if (i%105 == 0) {
           console.log("FizzBuzzBoom!");
       $("#output").append("<p>" + "FizzBuzzBoom!" + "</p>")
-
     } else if (i%35 == 0) {
           console.log("BuzzBoom!");
       $("#output").append("<p>" + "BuzzBoom!" + "</p>")
-
     } else if (i%21 == 0) {
           console.log("Buzz!");
       $("#output").append("<p>" + "FizzBoom!" + "</p>")
-
     } else if (i%15 == 0) {
           console.log("Boom!");
       $("#output").append("<p>" + "FizzBuzz!" + "</p>")
-
     } else if (i%7 == 0) {
           console.log("Boom!");
       $("#output").append("<p>" + "Boom!" + "</p>")
-
     } else if (i%5 == 0) {
           console.log("Buzz!");
       $("#output").append("<p>" + "Buzz!" + "</p>")
-
     } else if (i%3 == 0) {
           console.log("Fizz!");
       $("#output").append("<p>" + "Fizz!" + "</p>")
-
     } else {
           console.log(i);
       $("#output").append("<p>" + i + "</p>")
